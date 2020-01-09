5c636e48777d8e85adeb5739fe1dedf7b651c076
sinedied <noda@free.fr>
2020-01-09 09:15:52 +0100

    fix(): make request wrapper streamable

--- lib/router/azure-request.ts
+++ lib/router/azure-request.ts
@@ -1,13 +1,30 @@
-export class AzureRequest {
+import { Readable } from 'stream';
+
+export class AzureRequest extends Readable {
   readonly url: string;
   readonly context: Record<string, any>;
   readonly originalUrl: string;
   readonly headers: Record<string, any>;
+  readonly body: any;
 
   constructor(context: Record<string, any>) {
+    super();
+
     Object.assign(this, context.req);
     this.context = context;
     this.url = this.originalUrl;
     this.headers = this.headers || {};
+
+    // Recreate original request stream from body
+    const body =
+      context.req.body instanceof Buffer
+        ? context.req.body
+        : context.req.rawBody;
+
+    if (body !== null && body !== undefined) {
+      this.push(body);
+    }
+    // Close the stream
+    this.push(null);
   }
 }
