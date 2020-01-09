81d559023cfbd03d0b6196d9606dce60e7193990
sinedied <noda@free.fr>
2020-01-09 18:13:21 +0100

    fix(): wire streamed out data to body (close #89)

--- lib/adapter/azure-reply.ts
+++ lib/adapter/azure-reply.ts
@@ -1,10 +1,15 @@
 import { OutgoingMessage } from 'http';
 
 export class AzureReply extends OutgoingMessage {
+  private readonly _headerSent: boolean;
+  private readonly outputData: { data: any }[];
   statusCode?: number;
 
   constructor(context: Record<string, any>) {
     super();
+
+    // Avoid issues when data is streamed out
+    this._headerSent = true;
 
     this.writeHead = this.writeHead.bind(this, context);
     this.end = this.finish.bind(this, context);
@@ -31,6 +36,15 @@ export class AzureReply extends OutgoingMessage {
   }
 
   finish(context: Record<string, any>, body: Record<string, any> | undefined) {
+    // If data was streamed out, get it back to body
+    if (body === undefined && this.outputData.length > 0) {
+      body = Buffer.concat(
+        this.outputData.map(o =>
+          Buffer.isBuffer(o.data) ? o.data : Buffer.from(o.data)
+        )
+      );
+    }
+
     context.res.status = this.statusCode;
     context.res.body = body;
     context.done();
